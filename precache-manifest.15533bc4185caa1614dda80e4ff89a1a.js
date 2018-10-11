2d9a1f15a4c63e4286a98a1e3a89d06c7e0ec829
oleg <oleb140888@gmail.com>
2018-10-11 20:18:21 +0300

    Updates

---
+++ precache-manifest.15533bc4185caa1614dda80e4ff89a1a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "f400f8b6437091a2f6d7a04921d264a9",
+    "url": "/react-waxom/static/media/photo-mini3.f400f8b6.png"
+  },
+  {
+    "revision": "59869d037332d528c7a9",
+    "url": "/react-waxom/static/css/main.99fb8cd6.chunk.css"
+  },
+  {
+    "revision": "e945576979bce1b8321d49866a633d7f",
+    "url": "/react-waxom/static/media/photo6.e9455769.png"
+  },
+  {
+    "revision": "2af0695d68137217481b",
+    "url": "/react-waxom/static/js/1.2af0695d.chunk.js"
+  },
+  {
+    "revision": "90cc3583b7fd6143586b",
+    "url": "/react-waxom/static/js/runtime~main.90cc3583.js"
+  },
+  {
+    "revision": "c3928ba60f506939dc90db5e1dd57c8b",
+    "url": "/react-waxom/static/media/photo.c3928ba6.png"
+  },
+  {
+    "revision": "9bc054375d3f2722d57adc21425051b7",
+    "url": "/react-waxom/static/media/photo2.9bc05437.png"
+  },
+  {
+    "revision": "9faadc8eaf9ace18e2d82f312f93b3eb",
+    "url": "/react-waxom/static/media/photo3.9faadc8e.png"
+  },
+  {
+    "revision": "19a88428b7b54901dffa030b140e2933",
+    "url": "/react-waxom/static/media/photo-legs.19a88428.png"
+  },
+  {
+    "revision": "2b05b6e41575ad33e171493e1bf5046e",
+    "url": "/react-waxom/static/media/photo-pc.2b05b6e4.png"
+  },
+  {
+    "revision": "137404b3d542657590bdab150330a22a",
+    "url": "/react-waxom/static/media/photo-ship.137404b3.png"
+  },
+  {
+    "revision": "848c00f76bb645e4da1366cd2d69643f",
+    "url": "/react-waxom/static/media/photo5.848c00f7.png"
+  },
+  {
+    "revision": "82ec7bd0a23a4c499b74431f1901762a",
+    "url": "/react-waxom/static/media/realization-img.82ec7bd0.png"
+  },
+  {
+    "revision": "a9aa1b922f825bf7aa57cbfdd212c4cd",
+    "url": "/react-waxom/static/media/photo-mini1.a9aa1b92.png"
+  },
+  {
+    "revision": "23f40845d8191a424c1f4f711ae1e480",
+    "url": "/react-waxom/static/media/photo-mini2.23f40845.png"
+  },
+  {
+    "revision": "59869d037332d528c7a9",
+    "url": "/react-waxom/static/js/main.59869d03.chunk.js"
+  },
+  {
+    "revision": "6e4b0a8c2a5b8e4e7f759d001635b4d7",
+    "url": "/react-waxom/static/media/photo-mini4.6e4b0a8c.png"
+  },
+  {
+    "revision": "1ad91d8586954bb78a82435dbb3c3599",
+    "url": "/react-waxom/static/media/video-bg.1ad91d85.jpg"
+  },
+  {
+    "revision": "8c6bff84c9a6772d877b4f9a393e0895",
+    "url": "/react-waxom/static/media/iphone-in-hand.8c6bff84.png"
+  },
+  {
+    "revision": "86db57fad7d984d1af199bacd0c181a6",
+    "url": "/react-waxom/static/media/welcome-bg.86db57fa.jpg"
+  },
+  {
+    "revision": "f06c3a9f586e62786cfcb4c7297b3f3a",
+    "url": "/react-waxom/static/media/partners-bg.f06c3a9f.jpg"
+  },
+  {
+    "revision": "06f7ed254c7fb1b3ebe1dd05b5854da1",
+    "url": "/react-waxom/static/media/404.06f7ed25.jpg"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/react-waxom/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "575e4317521b381ac94c0c8207c81979",
+    "url": "/react-waxom/static/media/Raleway-Bold.575e4317.ttf"
+  },
+  {
+    "revision": "a38a54df0089bca4f5109fe44f944051",
+    "url": "/react-waxom/static/media/Raleway-ExtraBold.a38a54df.ttf"
+  },
+  {
+    "revision": "430a0518f5ff3b6c8968b759a29b36e2",
+    "url": "/react-waxom/static/media/Raleway-Medium.430a0518.ttf"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/react-waxom/static/media/Raleway-Regular.580d0778.ttf"
+  },
+  {
+    "revision": "17ba6410cbc694808961a988fd4426de",
+    "url": "/react-waxom/static/media/Raleway-SemiBold.17ba6410.ttf"
+  },
+  {
+    "revision": "272bf404d20d901424b66b7fa5f03a05",
+    "url": "/react-waxom/static/media/photo4.272bf404.png"
+  },
+  {
+    "revision": "2af0695d68137217481b",
+    "url": "/react-waxom/static/css/1.d841ddf3.chunk.css"
+  },
+  {
+    "revision": "02417df5ed6d2541aae55642aa13a339",
+    "url": "/react-waxom/index.html"
+  }
+];
